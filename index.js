92e5b562e2ad50875bead1674af615d3543ed869
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-10-31 01:57:25 +0400

    api url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://hollbeen-backend.onrender.com",
+        url: "http://hollbeen-backend.vercel.app",
       },
     ],
   },

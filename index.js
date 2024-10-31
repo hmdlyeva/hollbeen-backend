71be0fcc440a02a770d4d6394356143ad10f6ab6
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-10-31 09:29:16 +0400

    cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://hollbeen-backend.vercel.app",
+        url: "http://hollbeen-backend-v1.vercel.app",
       },
     ],
   },
